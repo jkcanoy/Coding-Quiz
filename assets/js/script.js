6b404c33a4930995123c712fc1425c23b73d14b0
Kyle Canoy <kcanoy@uw.edu>
2021-04-20 16:21:36 -0700

    added questions array

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// array of questions/options
+
+var questions = [
+    {
+        question: "What does DOM stand for?",
+        options: ["a. Document Object Model", "b. Data Ordered Modem", "c. Dumb Old Me", "d. Data Object Model"],
+        answer: "a. Document Object Model"
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["a. Holy Text Markup Language", "b. Hyper Text Makeup Language", "c. Hyper Text Markup Language", "d. How To Make Lyrics"],
+        answer: "c. Hyper Text Markup Language"
+    },
+    {
+        question: "____ is very useful tool to use when you do not understand something.",
+        options: ["a. Google", "b. Thoughts and Prayers", "c. Crying", "d. Giving Up"],
+        answer: "a. Google"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        options: ["a. loving arms", "b. quotes", "c. curly brackets", "d. parenthesis"],
+        answer: "b. quotes"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        options: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "d. all of the above"
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c. alerts"
+    },
+    {
+        question: "The first index of an array is ____.",
+        choices: ["a. first", "b. 1", "c. 8", "d. 0"],
+        answer: "d. 0"
+    },
+]
